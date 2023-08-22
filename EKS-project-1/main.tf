ca9cc2968d5b713d343e0a9b24f03d26bdb544f9
Mohamed Raiyan <mohamed_raiyan@epam.com>
2023-08-22 12:33:37 +0530

    re-factoring the code

--- EKS-project-1/main.tf
+++ EKS-project-1/main.tf
@@ -7,9 +7,10 @@ variable "vpc_cidr" {
   #default = "192.168.0.0/16"
 }
 
-variable "subnet_prefix" {
-  description = "subnet cidr with name seperated by comma"
-  #default = [{ cidr_block = "192.168.10.0/24", name = "pub_subA" }, { cidr_block = "192.168.20.0/24", name = "pub_subB" }]
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 # 1. Create VPC
@@ -18,31 +19,24 @@ resource "aws_vpc" "my_vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "eks-vpc"
+    Name = "terraform-eks",
+    # kubernetes.io/cluster/EKS-Cluster = "shared",
   }
 }
 
 # 2. Create Subnet
-resource "aws_subnet" "pub_subnetA" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.subnet_prefix[0].cidr_block
+resource "aws_subnet" "pub_subnet" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block              = "192.168.${count.index}.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "ap-southeast-1a"
-
+  vpc_id                  = aws_vpc.my_vpc.id
   tags = {
-    Name = var.subnet_prefix[0].name
+    Name = "terraform-eks",
+    # kubernetes.io/cluster/EKS-Cluster = "shared",
   }
 }
-resource "aws_subnet" "pub_subnetB" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.subnet_prefix[1].cidr_block
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-southeast-1b"
 
-  tags = {
-    Name = var.subnet_prefix[1].name
-  }
-}
 # 3. Create internet gateway
 resource "aws_internet_gateway" "myIGW" {
   vpc_id = aws_vpc.my_vpc.id
@@ -71,28 +65,22 @@ resource "aws_route_table" "pub-RT" {
 }
 # 5. Create route table association
 resource "aws_route_table_association" "pub-RT-association" {
-  subnet_id      = aws_subnet.pub_subnetA.id
+  count = "${length(data.aws_availability_zones.available.names)}"
+  subnet_id      = aws_subnet.pub_subnet.*.id[count.index]
   route_table_id = aws_route_table.pub-RT.id
 }
-resource "aws_route_table_association" "pubB-RT-association" {
-  subnet_id      = aws_subnet.pub_subnetB.id
-  route_table_id = aws_route_table.pub-RT.id
-}
+
 # Create SSH Keypair
 resource "aws_key_pair" "my-keypair" {
   key_name   = "tarraform-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCxCsaSC8vCHsmONR10Bz+GqvrMCM6+RpajuUcbRsBPkkYT+p8bNw5oDZhOdgsvCp+Blaj5LhHCjTZhGT3C1+LvRMwb4IZNdhfhy0SrQDtN0dQnJlgwnaLkXGMsQ6I6ehJkNeDQ0wU5NOTITOZ4Q4AOmlaVRvjWcHjmAg4jVkOOLBNsl42Cw0aRSgOMo5RQSAuINl8OF8qbXh9rMcdrApAdS2UvLaD1zxpfeAIOjfdF2ZQL0UDafwEuOnyVVRCdMDrt6VcyXWNyo7NCr85jUGMTieMuSVL6ARJ+Aer4npKLRb7P4tsgKgIQKhu5J8coAm/GmfoVeWisGRpqZiqb8dZ5OYT6yPK2XUEwi69kmffiEmyfMJtOoRyomsPwP93WrxUtaJA037bBB/FmwYv80Zgbic1ZRdO6DQ2pRDs4uZ6dSh2QHBMWstfNB80OJ1mjw8iVtV4G1Dd9oBMuoqYC5QeQGxUn5gwpH+vAG7yMnBXXLPKvfAJHkE6QjnO2WD0qevM= raiyan@DESKTOP-670C4M2"
 }
 
-variable "worker_instance_type" {
-  description = "worker instance type"
-  default     = "t3.medium"
-}
-
 locals {
   name            = "EKS-Cluster"
-  cluster_version = "1.23"
+  cluster_version = "1.24"
   region          = var.aws_region
+  instance_type   = "t3.medium"
 }
 
 module "eks" {
@@ -101,7 +89,7 @@ module "eks" {
 
   cluster_name                   = local.name
   cluster_version                = local.cluster_version
-  subnet_ids                     = [aws_subnet.pub_subnetA.id, aws_subnet.pub_subnetB.id]
+  subnet_ids                     = aws_subnet.pub_subnet[*].id
   vpc_id                         = aws_vpc.my_vpc.id
   cluster_endpoint_public_access = true
 
@@ -124,8 +112,8 @@ module "eks" {
     self_mg_4 = {
       node_group_name    = "self-managed-ondemand"
       launch_template_os = "amazonlinux2eks"
-      subnet_ids         = [aws_subnet.pub_subnetA.id, aws_subnet.pub_subnetB.id]
-      instance_type      = "t3.medium"
+      subnet_ids         = aws_subnet.pub_subnet[*].id
+      instance_type      = local.instance_type
       key_name           = aws_key_pair.my-keypair.key_name
       min_size           = 0
       max_size           = 3
